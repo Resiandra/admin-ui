c345e7b6144d8a165dd50b33392538aaefaa4fc3
Resiandra Bima <111202113665@mhs.dinus.ac.id>
2024-07-16 02:14:34 +0700

    ubah teks di app

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,7 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('http://localhost:5174/')
+describe('Categories Menu', () => {
+  it('should navigate to Categories page and perform CRUD operations', () => {
+    cy.visit('http://localhost:5173/')
+    cy.wait(3000); // Tunggu 3 detik setelah mengunjungi halaman
 
     cy.get('[data-testid="form"]').should("exist")
 
@@ -14,21 +15,31 @@ describe('template spec', () => {
       .should('have.attr', 'placeholder', 'Enter your password')
       .type('123456').should('have.value', '123456') 
       
-      cy.get('[data-testid="submit"]').click()
+    cy.get('[data-testid="submit"]').click()
+    cy.wait(3000); // Tunggu 3 detik setelah klik submit
 
-    cy.get('div.home')
+    cy.get('div.home').should('be.visible')
+    cy.get('div.sidebar').should('be.visible')
+
+    // 1) Mengklik menu Categories dan memastikan masuk ke halaman Categories
+    cy.get('[data-testid="categories"]', { timeout: 10000 }).should('be.visible').click()
+    cy.wait(3000); // Tunggu 3 detik setelah mengklik Categories
+    cy.url().should('include', '/categories')
+    cy.get('div.datatableTitle').contains('CATEGORIES')
+
+    // 2) Klik tombol Add New 
+    cy.get('[data-testid="add-new"]').click() // Ganti selector sesuai dengan tombol Add New
+    cy.wait(2000); // Tunggu 3 detik setelah mengklik Add New
+    
+    // 3) Tunggu dan menulis "Dessert" pada placeholder yang ada
+    cy.get('input[placeholder="Coffee"]', { timeout: 10000 }) // Ganti selector sesuai placeholder yang tepat
       .should('be.visible')
+      .type('Dessert')
+      .should('have.value', 'Dessert')
 
-      cy.get('div.sidebar')
-  .should('be.visible') 
-
-cy.get('[data-testid="users"]').click()
-cy.url().should('include', '/users')
-cy.get('div.datatableTitle').contains('USERS')
-
-
-
-
+    // 4) Klik tombol Send
+    cy.get('button[type="submit"]').click()
+ 
 
   })
 })
